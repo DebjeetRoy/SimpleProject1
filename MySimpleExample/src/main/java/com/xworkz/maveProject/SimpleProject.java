0fc7e97504352db7e8751c15005cbf792b38857f
DebjeetRoy <debjeetroy1992@gmail.com>
2019-09-21 16:34:59 +0530

    Added a commented line

--- MySimpleExample/src/main/java/com/xworkz/maveProject/SimpleProject.java
+++ MySimpleExample/src/main/java/com/xworkz/maveProject/SimpleProject.java
@@ -3,6 +3,6 @@ package com.xworkz.maveProject;
 public class SimpleProject {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome");
+		System.out.println("Welcome"); //this is added
 	}
 }
